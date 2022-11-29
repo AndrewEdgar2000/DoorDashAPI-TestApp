4f344b19477268c45b0a2859f4fb122ea145972a
Andrew Edgar <edgar.andrew@mail.com>
2022-11-29 13:50:01 -0800

    Started implementation of API

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 import path from "path"
 import {fileURLToPath} from "url"
-
+import "dotenv/config"
+import { v4 as uuidv4} from "uuid";
+import { DoorDashClient } from "@doordash/sdk";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,3 +22,21 @@ app.listen(port, (err) => {
     console.log('App listening on:', {port})
 });
 
+app.post("/get-delivery-rate", async (req, res) => {
+    const client = new DoorDashClient({
+        developer_id: process.env.DEVELOPER_ID,
+        key_id: process.env.KEY_ID,
+        signing_secret: process.env.SIGNING_SECRET,
+    });
+
+    const response = await client.deliveryQuote({
+        external_delivery_id: uuidv4(), 
+        pickup_address: '1000 4th Ave, Seattle, WA, 98104',
+        pickup_phone_number: '+1(650)5555555',
+        dropoff_address: '1201 3rd Ave, Seattle, WA, 98101',
+        dropoff_phone_number: '+1(650)5555555',
+    })
+
+    res.send(response);
+
+});
