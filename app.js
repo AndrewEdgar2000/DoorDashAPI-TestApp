b2427680e44893fac05c1d75bcaa2fca54afb024
Andrew Edgar <edgar.andrew@mail.com>
2022-11-29 18:47:12 -0800

    Implementing some of the front end

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import {fileURLToPath} from "url"
 import "dotenv/config"
 import { v4 as uuidv4} from "uuid";
 import { DoorDashClient } from "@doordash/sdk";
+import { execArgv } from "process";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,6 +23,8 @@ app.listen(port, (err) => {
     console.log('App listening on:', {port})
 });
 
+app.use(express.static(__dirname + "/public"))
+
 app.post("/get-delivery-rate", async (req, res) => {
     const client = new DoorDashClient({
         developer_id: process.env.DEVELOPER_ID,
@@ -34,9 +37,23 @@ app.post("/get-delivery-rate", async (req, res) => {
         pickup_address: '1000 4th Ave, Seattle, WA, 98104',
         pickup_phone_number: '+1(650)5555555',
         dropoff_address: '1201 3rd Ave, Seattle, WA, 98101',
-        dropoff_phone_number: '+1(650)5555555',
+        dropoff_phone_number: '+1(650)5555523',
     })
 
     res.send(response);
-
+    console.log("QUOTE", response);
 });
+
+app.post("/create-delivery", async (req, res) => {
+    const client = new DoorDashClient({
+        developer_id: process.env.DEVELOPER_ID,
+        key_id: process.env.KEY_ID,
+        signing_secret: process.env.SIGNING_SECRET,
+    });
+    const response = await client.deliveryQuoteAccept(
+        "9afeaa19-210d-46a4-965b-3a6e08ec8f60"
+    );
+
+    res.send(response);
+    console.log("ACCEPT", response);
+})
