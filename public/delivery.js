ea3e7eb8d82468278f730f438511384bdc679733
Andrew Edgar <edgar.andrew@mail.com>
2022-12-11 16:55:31 -0800

    Async funtion not being read through html

--- public/delivery.js
+++ public/delivery.js
@@ -1,3 +1,5 @@
+import { response } from "express";
+
 function getFormValues(){
     const inputContainer = document.getElementById("order-details");
     const fileList = inputContainer.querySelectorAll("input");
@@ -18,4 +20,37 @@ function getFormValues(){
     );
     console.log(payload);
     return payload;
+}
+
+async function getFee(){
+    const payload = getFormValues()
+    const finalPayload = JSON.stringify(payload)
+
+    const formInput = document.querySelector("form")
+
+    if(formInput.checkValidity()){
+        const response = await fetch("/get-delivery-rate",{
+            method: "POST", 
+            body: finalPayload,
+            headers: {"Content-Type" : "application/json"} 
+        }).then(response => {
+            let resp = response.json()
+            return resp
+        }).catch((rejected) => {
+            console.log(rejected)
+        })
+
+        const deliveryFee = document.getElementById("fee")
+        const clothingTotal = document.getElementById("price")
+        const orderTotal = document.getElementById("total")
+    
+        clothingTotal.textContent =`$${(window.menuItems / 100).toFixed(2)}`
+        deliveryFee.textContent = `$${(response.data.fee / 100).toFixed(2)}`
+        orderTotal.textContent = `$${((Number(window.menuItems) + response.data.fee) / 100).toFixed(2)}`
+    
+        console.log("I filled it out")
+    } else {
+        console.log("Fill out the form")
+    }
+    
 }
